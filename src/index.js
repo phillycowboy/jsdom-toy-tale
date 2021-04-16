604512ee6b5ad40a455f428002582189c90629ee
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-16 17:48:19 -0400

    commit tree 87d032351f22c45d1db6ab5b51e274368ac10dab

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  // const toyDiv = document.querySelector('#toy-collection');
+  // const baseUrl = "https://localhost:3000/toys";
+
+  // function getToys() {
+  //   return fetch(baseUrl)
+  //   .then((res) => {
+  //     return res.json()
+  //   }).then((toys) => {
+  //     console.log(toys);
+  //   })
+
+    
+  // }
+  
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +26,23 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+  
+  
+// });
+
+const toyDiv = document.querySelector('#toy-collection');
+const baseUrl = "http://localhost:3000/toys";
+
+function getToys() {
+return fetch(baseUrl)
+  .then(function(res) {
+    return res.json();
+  }).then(function(object) {
+    console.log(object);
+  }).catch(function(error){
+      console.log(error);
+  })
+}
+
+getToys();  
