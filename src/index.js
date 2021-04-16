d40518aba1969405e0c88c4047f0e45885ddc5f7
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-16 17:57:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  // const toyDiv = document.querySelector('#toy-collection');
+  // const baseUrl = "https://localhost:3000/toys";
+
+  // function getToys() {
+  //   return fetch(baseUrl)
+  //   .then((res) => {
+  //     return res.json()
+  //   }).then((toys) => {
+  //     console.log(toys);
+  //   })
+
+    
+  // }
+  
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +26,21 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  
+  
+  const toyDiv = document.querySelector('#toy-collection');
+  const baseUrl = "http://localhost:3000/toys";
+  
+  function getToys() {
+     return fetch("http://google.com").then(function(res) {
+     return res.json();
+    }).then(function(object) {
+      console.log(object);
+    }).catch(function(error){
+        console.log(error);
+    })
+  }
+  getToys();
+  
 });
