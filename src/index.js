b37cc7a8147a30ce08fe2ceedb2cd221d7106727
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-18 13:45:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,47 @@
 let addToy = false;
 
+const toyDiv = document.querySelector('#toy-collection');
+const baseUrl = "http://localhost:3000/toys";
+
+function getToys() {
+   return fetch(baseUrl).then(function (res) {
+     return res.json();
+  }).then(function (json) {
+    toys = json
+    toys.forEach(toy => renderToys(toy))
+  }).catch(function (error) {
+    console.log(error);
+  })
+}
+
+function renderToys(toy) {
+  console.log(toy);
+    const newToy = document.createElement('div')
+    newToy.innerText = toy.name
+    toyDiv.append(newToy)
+}
+
+getToys();
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-  // const toyDiv = document.querySelector('#toy-collection');
-  // const baseUrl = "https://localhost:3000/toys";
-
-  // function getToys() {
-  //   return fetch(baseUrl)
-  //   .then((res) => {
-  //     return res.json()
-  //   }).then((toys) => {
-  //     console.log(toys);
-  //   })
-
-    
-  // }
   
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
+          // hide & seek with the form
+          addToy = !addToy;
+          if (addToy) {
+            toyFormContainer.style.display = "block";
+          } else {
+            toyFormContainer.style.display = "none";
+          }
+        });
+        
+        
+        
 
-  
-  
-  const toyDiv = document.querySelector('#toy-collection');
-  const baseUrl = "http://localhost:3000/toys";
-  
-  function getToys() {
-     return fetch("http://google.com").then(function(res) {
-     return res.json();
-    }).then(function(object) {
-      console.log(object);
-    }).catch(function(error){
-        console.log(error);
-    })
-  }
-  getToys();
-  
+
 });
+              
