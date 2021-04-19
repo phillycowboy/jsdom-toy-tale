23a80e7ec340e4841b2e8a11578a2df34e059160
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-19 13:07:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -38,24 +38,23 @@ function renderToys(toy) {
   toyDiv.append(newToy);
 }
 
-function createToy(name, image, likes) {
-  console.log("I was clicked");
-  console.log("image", image)
-  console.log("likes", likes)
+function createToy(formData) {
+  console.log("This is the form", formData.value);
+
   let configObj = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
-    body: JSON.stringify({"name": name, "image": image, "likes": likes})
+    body: JSON.stringify({"name": formData[0].value, "image": formData[1].value, "likes": 0})
   };
    return fetch(baseUrl, configObj)
   .then(function (res){
     return res.json()
   })
   .then(function (json){
-    renderToys({id: json.id, "name": name, "image": image, "likes": likes});
+    renderToys({id: json.id, "name": formData[0].value, "image": formData[1].value, "likes": 0});
   }) 
   .catch(function(error){
     console.log(error)
@@ -66,41 +65,25 @@ function createToy(name, image, likes) {
 getToys();
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  const submitBtn = document.querySelector("#submit");
-  const toyForm = document.querySelector('#add-new-toy');
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+const submitBtn = document.querySelector("#submit");
+const toyForm = document.querySelector('#add-new-toy');
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
 
-  toyForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    // // console.log(toy);
-    // let name = e.target[0].value;
-    // let image = e.target[1].value;
-    // const newToy = document.createElement('div');
-    // const h2 = document.createElement('h2');
-    // const img = document.createElement('img');
-    // const p = document.createElement('p');
-    // const btn = document.createElement('button');
-    // newToy.classList = "card";
-    // h2.innerText = name;
-    // img.src = image;
-    // // p.innerText = likes;
-    // img.classList = "toy-avatar";
-    // btn.classList = "like-btn";
-    // btn.innerText = "LIKE!!";
-    // newToy.append(h2);
-    // newToy.append(img);
-    // newToy.append(p);
-    // newToy.append(btn);
-    // toyDiv.append(newToy);
-    createToy();
+toyForm.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  console.log(e.target[0].value);
+  console.log(e.target[1].value);
+  console.log(e.target[2].value);
+  // console.log(e.target[0].value)
+   createToy(e.target);
     
-  })
+});
+  document.addEventListener("DOMContentLoaded", () => {
   
   addBtn.addEventListener("click", () => {
-          // hide & seek with the form
-          // console.log(submitBtn);
+    // hide & seek with the form
+    // console.log(submitBtn);
           addToy = !addToy;
           if (addToy) {
             toyFormContainer.style.display = "block";
